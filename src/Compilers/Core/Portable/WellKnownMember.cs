373490c1f7a05f3a545df908ed4568e8ab321009
Julien Couvreur <12466233+jcouv@users.noreply.github.com>
2026-03-19 19:16:37 -0700

    Optimize codegen for slicing to end (#82729)
    Co-authored-by: Tatsunori Uchino <tats.u@live.jp>
    Co-authored-by: kasperk81 <83082615+kasperk81@users.noreply.github.com>

--- src/Compilers/Core/Portable/WellKnownMember.cs
+++ src/Compilers/Core/Portable/WellKnownMember.cs
@@ -723,6 +723,13 @@ namespace Microsoft.CodeAnalysis
         System_Text_Encoding__get_UTF8,
         System_Text_Encoding__GetString,
 
+        System_Span_T__Slice_Int,
+        System_ReadOnlySpan_T__Slice_Int,
+        System_Memory_T__Slice_Int_Int,
+        System_Memory_T__Slice_Int,
+        System_ReadOnlyMemory_T__Slice_Int_Int,
+        System_ReadOnlyMemory_T__Slice_Int,
+
         System_Runtime_CompilerServices_UnionAttribute__ctor,
 
         Count,
